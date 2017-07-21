c2cfe653135f54fb93f8fa147f767fd43230de6f
majid-mashhadi <majid1347@yahoo.com>
2017-07-21 12:42:28 -0700

    Add files via upload

---
+++ RequireHttpsAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Net.Http;
+using System.Net;
+
+namespace WebAPI
+{
+    public class RequireHttpsAttribute: AuthorizationFilterAttribute 
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.RequestUri.Scheme != Uri.UriSchemeHttps)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Found);
+                actionContext.Response.Content = new StringContent("<p>HTTPS required</p>",System.Text.Encoding.UTF8,"text/html");
+                UriBuilder uriBuilder = new UriBuilder(actionContext.Request.RequestUri);
+                uriBuilder.Scheme = Uri.UriSchemeHttps;
+                uriBuilder.Port = 44379; // Port for the https
+                actionContext.Response.Headers.Location = uriBuilder.Uri;
+            }
+            else
+            {
+                base.OnAuthorization(actionContext);
+            }
+        }
+    }
+}
